4671bf02135fd46a17940cc15ec637c618aa5c72
KisaragiEffective <kisaragi.effective@gmail.com>
2019-06-16 00:41:54 +0900

    changed some signature

--- src/main/java/com/github/kisaragieffective/opencommandblock/api/selector/Selector.kt
+++ src/main/java/com/github/kisaragieffective/opencommandblock/api/selector/Selector.kt
@@ -1,12 +1,12 @@
 package com.github.kisaragieffective.opencommandblock.api.selector
 
 import com.github.kisaragieffective.opencommandblock.api.common.TargetVersionRange
-import com.github.kisaragieffective.opencommandblock.api.selector.value.SelectorValues
+import com.github.kisaragieffective.opencommandblock.api.selector.value.ISelectorPropertySet
 
 interface Selector {
     val type: SelectorType
 
-    val values: SelectorValues
+    val values: ISelectorPropertySet
 
     val applicableServerVersion: TargetVersionRange
 }
--- src/main/java/com/github/kisaragieffective/opencommandblock/api/selector/SelectorImpl12.kt
+++ src/main/java/com/github/kisaragieffective/opencommandblock/api/selector/SelectorImpl12.kt
@@ -2,46 +2,55 @@ package com.github.kisaragieffective.opencommandblock.api.selector
 
 import com.github.kisaragieffective.opencommandblock.api.common.TargetVersionRange
 import com.github.kisaragieffective.opencommandblock.api.common.Version
-import com.github.kisaragieffective.opencommandblock.api.selector.value.SelectorValues
-import org.bukkit.World
+import com.github.kisaragieffective.opencommandblock.api.selector.value.ISelectorPropertySet
+import com.github.kisaragieffective.opencommandblock.api.selector.value.SelectorPropertySetImpl12
 import kotlin.math.min
 import com.github.kisaragieffective.opencommandblock.kotlinmagic.extension.get
+import org.bukkit.Location
 import java.util.EnumSet
 
-class SelectorImpl12(override val type: SelectorType, override val values: SelectorValues) : Selector {
+class SelectorImpl12(override val type: SelectorType, override val values: ISelectorPropertySet) : Selector {
     override val applicableServerVersion: TargetVersionRange
         get() = TargetVersionRange(Version(1, 12, 0), Version(1, 12, 2))
     companion object {
-        val arrowedType: EnumSet<SelectorType> = EnumSet.of(SelectorType.ALL, SelectorType.ENTITY, SelectorType.PERSONAL, SelectorType.RANDOM)
+        val arrowedType = EnumSet.of(SelectorType.ALL, SelectorType.ENTITY, SelectorType.PERSONAL, SelectorType.RANDOM)
 
-        fun parse(world: World, input: String): SelectorImpl12 {
+        fun parse(base: Location, input: String): SelectorImpl12 {
             if (input.isEmpty()) {
-                throwException("Input is Empty")
+                parseError("Input is Empty")
             }
 
             if (input.length <= 1) {
-                throwException("At least 2 chars needed")
+                parseError("At least 2 chars needed")
             }
 
             if (input[0] != '@') {
-                throwException("Selector always starts with '@'")
+                parseError("Selector always starts with '@'")
             }
 
             val arrowStringType = arrowedType.map { it.s }
             if (!arrowStringType.contains(input[0..1])) {
-                throwException("Selector type must be either one of ${arrowStringType.joinToString(", ")}")
+                parseError("Selector type must be either one of ${arrowStringType.joinToString(", ")}")
             }
 
-            if ()
+            val type = input[0..1]
+
+            // @[arpe]
+            if (input.length == 2 && arrowStringType.contains(type)) {
+                return SelectorImpl12(SelectorType.values().find { it.s == type } ?: parseError("$type not found"), SelectorPropertySetImpl12(
+                        base
+                ))
+            }
+            TODO()
         }
 
-        private fun throwException(message: String, input: String, erroredRange: IntRange): Nothing {
+        private fun parseError(message: String, input: String, erroredRange: IntRange): Nothing {
             val zo = input.substring(min(0, erroredRange.first), min(erroredRange.endInclusive, input.length))
             val tk = "^" + "~".repeat(zo.length)
             throw SelectorSyntaxException("Syntax Error: $message\n$zo\n$tk")
         }
 
-        private fun throwException(message: String): Nothing {
+        private fun parseError(message: String): Nothing {
             throw SelectorSyntaxException("Syntax Error: $message")
         }
     }

be1170be17899aa887e372e4e7445e3ab033bf81
KisaragiEffective <kisaragi.effective@gmail.com>
2019-07-16 21:54:05 +0900

    UNDER CONSTRUCTION

--- src/main/java/com/github/kisaragieffective/opencommandblock/command/impl/TestSelectorQuery.kt
+++ src/main/java/com/github/kisaragieffective/opencommandblock/command/impl/TestSelectorQuery.kt
@@ -1,18 +1,14 @@
 package com.github.kisaragieffective.opencommandblock.command.impl
 
 import com.github.kisaragieffective.opencommandblock.OpenCommandBlock
-import com.github.kisaragieffective.opencommandblock.api.selector.SelectorBuilder
-import com.github.kisaragieffective.opencommandblock.api.selector.SelectorOrder
-import com.github.kisaragieffective.opencommandblock.api.selector.SelectorQueryExecutor
 import com.github.kisaragieffective.opencommandblock.command.CommandArgumentType
 import com.github.kisaragieffective.opencommandblock.command.PlayerCommandListener
-import com.github.kisaragieffective.opencommandblock.kotlinmagic.extension.each2common.toFrameworkStyle
 import org.bukkit.command.Command
 import org.bukkit.entity.Player
 
 object TestSelectorQuery : PlayerCommandListener {
     override fun firedCommand(sender: Player, command: Command?, alias: String?, args: Array<out String>): Boolean {
-        SelectorQueryExecutor.apply(sender.world.entities, SelectorBuilder().setMaximumRange(60).setLimit(5, SelectorOrder.NEAREST), sender.location.toVector().toFrameworkStyle())
+        sender.sendMessage("This command is arity; Selector API is Under Constructing")
         return true
     }
 

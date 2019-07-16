c0218a8c65a71e0266188ba28777b4ec55fc0f97
KisaragiEffective <kisaragi.effective@gmail.com>
2019-07-16 21:53:37 +0900

    parameter rename

--- src/main/java/com/github/kisaragieffective/opencommandblock/api/selector/distance/IDistanceArea.kt
+++ src/main/java/com/github/kisaragieffective/opencommandblock/api/selector/distance/IDistanceArea.kt
@@ -1,7 +1,8 @@
 package com.github.kisaragieffective.opencommandblock.api.selector.distance
 
+import com.github.kisaragieffective.opencommandblock.interfaces.MustImplementContains
 import org.bukkit.entity.Entity
 
-interface IDistanceArea {
-    operator fun contains(e: Entity): Boolean
+interface IDistanceArea : MustImplementContains<Entity> {
+    override operator fun contains(other: Entity): Boolean
 }

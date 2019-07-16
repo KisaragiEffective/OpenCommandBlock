8b8a652125317d6e21ac0b55794aeef6cc69601f
KisaragiEffective <kisaragi.effective@gmail.com>
2019-07-16 22:58:30 +0900

    debug friendly

--- src/main/java/com/github/kisaragieffective/opencommandblock/event/CommandBlockPlaceEvent.kt
+++ src/main/java/com/github/kisaragieffective/opencommandblock/event/CommandBlockPlaceEvent.kt
@@ -4,4 +4,7 @@ import org.bukkit.block.CommandBlock
 import org.bukkit.entity.Player
 
 class CommandBlockPlaceEvent(block: CommandBlock, val player: Player) : CommandBlockEvent(block) {
+    override fun toString(): String {
+        return "$eventName{block=${getCommandBlock()},who=$player}"
+    }
 }
--- src/main/java/com/github/kisaragieffective/opencommandblock/listener/DebugListener.kt
+++ src/main/java/com/github/kisaragieffective/opencommandblock/listener/DebugListener.kt
@@ -1,7 +1,11 @@
 package com.github.kisaragieffective.opencommandblock.listener
 
 import com.github.kisaragieffective.opencommandblock.OpenCommandBlock
+import com.github.kisaragieffective.opencommandblock.event.CommandBlockBreakEvent
+import com.github.kisaragieffective.opencommandblock.event.CommandBlockCommandChangeEvent
 import com.github.kisaragieffective.opencommandblock.event.CommandBlockPlaceEvent
+import com.github.kisaragieffective.opencommandblock.event.CommandBlockPowerEvent
+import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -9,6 +13,25 @@ import org.bukkit.event.Listener
 object DebugListener : Listener {
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     fun onCommandBlockPlace(e: CommandBlockPlaceEvent) {
-        OpenCommandBlock.instance.logger.info("[DEBUG/Event] $e")
+        o(e)
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    fun onCommandBlockBreak(e: CommandBlockBreakEvent) {
+        o(e)
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    fun onCommandBlockCommandChange(e: CommandBlockCommandChangeEvent) {
+        o(e)
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    fun onCommandBlockPower(e: CommandBlockPowerEvent) {
+        o(e)
+    }
+
+    private fun o(e: Event) {
+        OpenCommandBlock.instance.value!!.logger.info("[DEBUG/Event] $e")
     }
 }
